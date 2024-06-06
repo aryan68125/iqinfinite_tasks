3047a9f5101fd4557ec973960c7aa40876994470
Aditya Kumar <aryan68125@gmail.com>
2024-06-06 10:33:04 +0530

    added an api end-point to remove Assigned employees from the hr to the bruno collection

--- iqinfinite_project/EmployeeManager/static/js/admin_app/admin_settings_page.js
+++ iqinfinite_project/EmployeeManager/static/js/admin_app/admin_settings_page.js
@@ -1,3 +1,22 @@
 $('body').ready(function(){
     console.log("admin settings page js ready!!")
 })
+
+// GET CSRF TOKEN FROM THE BROWSER'S COOCKIE STARTS
+function getCookie(name) {
+    let cookieValue = null;
+    if (document.cookie && document.cookie !== "") {
+      const cookies = document.cookie.split(";");
+      for (let i = 0; i < cookies.length; i++) {
+        const cookie = cookies[i].trim();
+        // Does this cookie string begin with the name we want?
+        if (cookie.substring(0, name.length + 1) === (name + "=")) {
+          cookieValue = decodeURIComponent(cookie.substring(name.length + 1));
+          break;
+        }
+      }
+    }
+    return cookieValue;
+  }
+// GET CSRF TOKEN FROM THE BROWSER'S COOCKIE ENDS
+
