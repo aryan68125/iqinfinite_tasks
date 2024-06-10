a5cd0129f75a6ca53c66851d1b2d576bf0af6731
Aditya Kumar <aryan68125@gmail.com>
2024-06-10 18:29:34 +0530

    last commit

--- iqinfinite_project/EmployeeManager/auth_user/tasks.py
+++ iqinfinite_project/EmployeeManager/auth_user/tasks.py
@@ -2,10 +2,11 @@ from __future__ import absolute_import , unicode_literals
 from celery import shared_task
 from celery.utils.log import get_task_logger
 from auth_user.send_email import *
+logger  = get_task_logger(__name__)
 @shared_task(name='add')
 def add(x, y):
     return x + y
-logger  = get_task_logger(__name__)
+
 @shared_task(name="send_email_task")
 def send_email_task(subject, message, EMAIL_HOST_USER, recipient_list):
     logger.info("Mail sent")
---
+++ iqinfinite_project/EnquiryWebApp/auth_app/tasks.py
@@ -0,0 +1,13 @@
+from __future__ import absolute_import , unicode_literals
+from celery import shared_task
+from celery.utils.log import get_task_logger
+# from auth_app.send_email import *
+logger  = get_task_logger(__name__)
+@shared_task(name='add')
+def add(x, y):
+    return x + y
+
+# @shared_task(name="send_email_task")
+# def send_email_task(subject, message, EMAIL_HOST_USER, recipient_list):
+#     logger.info("Mail sent")
+#     return send_email(subject, message, EMAIL_HOST_USER, recipient_list)
